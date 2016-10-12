32fbb8ca005fed23ca1d86f921f352796bbc39a7
Melissa Garcia <melissag13@gmail.com>
2016-10-11 21:14:58 -0400

    fix server.js code

--- server.js
+++ server.js
@@ -1,41 +1,9 @@
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override')
-// var app = express();
-// var PORT = process.env.PORT || 3000;
-
-// var db = process.env.DATABASE_URL || 'localhost'
-
-// //Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(process.cwd() + '/public'));
-
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({
-// 	extended: false
-// }))
-
-// // override with POST having ?_method=DELETE
-// app.use(methodOverride('_method'))
-// var exphbs = require('express-handlebars');
-// app.engine('handlebars', exphbs({
-//     defaultLayout: 'main'
-// }));
-// app.set('view engine', 'handlebars');
-
-// var routes = require('./controllers/burgers_controller.js');
-// app.use('/', routes);
-// app.use('/update', routes);
-// app.use('/create', routes);
-
-// app.listen(PORT);
-
-// console.log(module.exports)
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
-
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
+var PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
@@ -56,7 +24,6 @@ app.use('/', routes);
 app.use('/update', routes);
 app.use('/create', routes);
 
-var port = 3000;
-app.listen(port);
+app.listen(PORT);
 
 console.log(module.exports)
